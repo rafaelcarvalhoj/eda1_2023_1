2e1732a8bad8c248f39db9569d3b19f8de954f5c
¨rafaelcarvalhoj¨ <rrafelcarvalhoj@gmail.com>
2023-06-11 12:18:54 -0300

    Quick-sort

--- sort/quick_sort.c
+++ sort/quick_sort.c
@@ -6,6 +6,18 @@ typedef int TYPE;
 #define exch(A, B) {TYPE temp = A; A = B; B = temp;}
 #define compexch(A, B) if(less(B, A)) exch(A, B) //if B < A, exch
 
+int partitionRibas(TYPE a[], int l, int r)
+{   int size = r-l+1;
+    TYPE smaller = malloc(sizeof(TYPE)*size);
+    TYPE bigger = malloc(sizeof(TYPE)*size);
+    for(int i = l; i < r ; i++)
+    {
+        
+    }
+}
+
+
+
 int partition(TYPE a[], int l, int r)
 {	int i = l-1, j = r; TYPE v = a[r];
 
